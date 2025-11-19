f093e4f5af76df1183b58018172a55f98f377180
Juhun Park <riann3207@gmail.com>
2025-11-19 11:34:35 -0500

    feat: in progress: google oauth2,0

---
+++ go/cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+func main() {
+	r := gin.Default()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env file failed to load!")
+	}
+
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	clientCallbackURL := os.Getenv("CLIENT_CALLBACK_URL")
+
+	if clientID == "" || clientSecret == "" || clientCallbackURL == "" {
+		log.Fatal("Environment variables (CLIENT_ID, CLIENT_SECRET, CLIENT_CALLBACK_URL) are required")
+	}
+
+	goth.UseProviders(
+		google.New(clientID, clientSecret, clientCallbackURL),
+	)
+
+	r.GET("/", home)
+	r.GET("/auth/:provider", signInWithProvider)
+	r.GET("/auth/:provider/callback", callbackHandler)
+
+	r.Run(":5000")
+}
+
+func home(c *gin.Context) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(c.Writer, gin.H{})
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+}
+
+func signInWithProvider(c *gin.Context) {
+
+	provider := c.Param("provider")
+	q := c.Request.URL.Query()
+	q.Add("provider", provider)
+	c.Request.URL.RawQuery = q.Encode()
+
+	gothic.BeginAuthHandler(c.Writer, c.Request)
+}
+
+func callbackHandler(c *gin.Context) {
+
+	provider := c.Param("provider")
+	q := c.Request.URL.Query()
+	q.Add("provider", provider)
+	c.Request.URL.RawQuery = q.Encode()
+
+	_, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.Redirect(http.StatusTemporaryRedirect, "/")
+}
